4cbcd8887be18341f078ae801c13fee5c7c756d6
nischitha-gudigar <nischithagudigardr@gmail.com>
2021-08-26 21:02:23 +0530

    2 files modified

--- src/app/my-counter/my-counter.component.ts
+++ src/app/my-counter/my-counter.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { filter, map, skip, startWith, take } from 'rxjs/operators';
 import { increment, decrement, reset } from '../counter.actions';
 
 @Component({
@@ -14,6 +14,34 @@ export class MyCounterComponent {
 
   constructor(private store: Store<{ count: number }>) {
     this.count$ = store.select('count');
+    //Display squared value for each like 0,4,9
+    this.count$
+      .pipe(map(res => res * res))
+      .subscribe(value =>
+        console.log('using map operator print squared numbers', value)
+      );
+
+    //Display only the first 3 values emitted.
+    this.count$
+      .pipe(take(3))
+      .subscribe(value => console.log('Only emit first 3 using take', value));
+
+    //Display only values emitted that are even numbers:
+    this.count$
+      .pipe(filter(res => res % 2 === 0))
+      .subscribe(value =>
+        console.log('Only emit required values using filter', value)
+      );
+
+    // Display all values emitted but the 3 first
+    this.count$
+      .pipe(skip(3))
+      .subscribe(value => console.log('skip of first three values', value));
+
+    //Make the observable emit a first value equal to 100
+    this.count$
+      .pipe(startWith(100))
+      .subscribe(value => console.log('start with 100', value));
   }
 
   increment() {
